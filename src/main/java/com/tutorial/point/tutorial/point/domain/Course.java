271a2bf7b0e0fe8df2b385ae2c7ff514a6f5ab2b
Jaspayu <jaspayal231198@gmail.com>
2021-12-14 11:37:25 +0530

    Implemented Course Service with Exceptions

--- src/main/java/com/tutorial/point/tutorial/point/domain/Course.java
+++ src/main/java/com/tutorial/point/tutorial/point/domain/Course.java
@@ -1,17 +1,20 @@
 package com.tutorial.point.tutorial.point.domain;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.stereotype.Component;
 
+
 @NoArgsConstructor
 @AllArgsConstructor
 @Component
 @Data
 @Document(collection = "Course")
+@Builder
 public class Course {
     @Id
     String courseId;
--- src/test/java/com/tutorial/point/tutorial/point/TutorialPointApplicationTests.java
+++ src/test/java/com/tutorial/point/tutorial/point/TutorialPointApplicationTests.java
@@ -1,7 +1,10 @@
 package com.tutorial.point.tutorial.point;
 
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
 
 @SpringBootTest
 class TutorialPointApplicationTests {
